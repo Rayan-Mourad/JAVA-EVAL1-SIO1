da942be358a0811cd1c63c2c6254d8aed335c69f
rayan <rayan.lacheheb@ortmontreuil.fr>
2024-10-28 21:33:38 +0100

    EvalRayan bonne note jpense

--- src/main/java/com/sio/Main.java
+++ src/main/java/com/sio/Main.java
@@ -1,5 +1,7 @@
 package com.sio;
 
+package com.sio;
+
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
@@ -33,6 +35,18 @@ public class Main {
                     System.out.println("Exercice 1");
                     System.out.println("---------------------------------------------");
                     //Exercice 1 : Entrer votre code ici
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.print("Entrer un nombre");
+                    int nombre = scanner.nextInt();
+                    System.out.print("Entrer une puissance");
+                    double Puissance = scanner.nextInt();
+                    Scanner.nextInt();
+                    System.out.print("La puissance doit etre superieur ou egale à 0");
+                    System.out.print("Entrer une valeur");
+                    double resultat = Math.pow(nombre, Puissance);
+                    System.out.print("le resultat de "nombre "a la puissance "Puissance"est" resultat);
+
+
 
 
 
@@ -44,6 +58,32 @@ public class Main {
                     System.out.println("---------------------------------------------");
                     //Exercice 2 : Entrer votre code ici
 
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.print("Entrer le nombre de kilomètres parcourus par le client: ");
+                    int kilomètres = scanner.nextInt();
+                    double tarifparkm = 0.20;
+                    double montantPayer = kilomètres * tarifparkm ;
+                    System.out.print("Le montant à payer par le client est :" montantPayer);
+
+                    System.out.print("Entrer le nombre de kilomètres parcourus par le client: ");
+                    int kilomètres = scanner.nextInt();
+                    double tarifparkm = 0.50;
+                    double montantPayer = kilomètres * tarifparkm ;
+                    System.out.print("Le montant à payer par le client est :" montantPayer);
+
+                    System.out.print("Entrer le nombre de kilomètres parcourus par le client: ");
+                    int kilomètres = scanner.nextInt();
+                    System.out.print("Le nombre de kilomètres doit etre superieur à 0");
+                    System.out.print("Entrer le nombre de kilomètres parcourus par le client: ");
+                    double kilparcourus = scanner.nextInt();
+                    double montantPayer = kilomètres - 102,40
+                    System.out.print("Le montant à payer par le client est:" montantPayer);
+
+
+
+
+
+
 
 
 
@@ -54,6 +94,20 @@ public class Main {
                     System.out.println("Exercice 3");
                     System.out.println("---------------------------------------------");
                     //Exercice 3 : Entrer votre code ici
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.print("Entrer la longueur du rectangle :");
+                    Int longueur = scanner.nextInt();
+                    System.out.print("la largeur doit etre superieur ou egale à 0");
+                    System.out.print("Entrer la largeur du rectangle:");
+                    Int largeur = scanner.nextInt();
+                    System.out.print("Entrer la hauteur du rectangle:");
+                    Int hauteur = scanner.nextInt();
+                    System.out.print("La hauteur doit etre supérieur ou égale à 0 ");
+                    System.out.print("Entrer la hauteur du rectangle");
+                    Int hauteur = scanner.nextInt();
+
+
+
 
 
 
@@ -66,6 +120,24 @@ public class Main {
                     System.out.println("Exercice 4");
                     System.out.println("---------------------------------------------");
                     //Exercice 4 : Entrer votre code ici
+                    Scanner.scanner = new Scanner(System.in);
+                    System.out.print("Entrer un nombre :");
+                    Int nombre = scanner.nextInt();
+                    System.out.print("Le nombre doit etre superieur ou égale a 0");
+                    System.out.print("Entrer un nombre :");
+                    int factoriel = scanner.nextInt();
+                    char factoriel = n;
+                    if (n > 0);
+                    do n-- ;
+                    else ;
+                    n = 0;
+                    System.out.print(" Le factoriel de "nombre" est" n);
+
+
+
+
+
+
 
 
 
